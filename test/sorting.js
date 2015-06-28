ca9942288b5ae79f6632ad2e7f712abdfb7bbece
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2015-06-28 13:08:52 -0300

    Complete tests for bubbleSort, insertionSort, selectionSort

--- test/sorting.js
+++ test/sorting.js
@@ -27,10 +27,12 @@ var objectArray = [{
     yearsUntilGraduation: 0
 }];
 
-describe('Bubble Sorting', function () {
+describe('Bubble Sort', function () {
   it('Should sort the simple array correctly', function () {
     var sorter = new Sorter(aSimpleArray);
+
     assert.equal(sorter.bubbleSort().length, 6);
+
     for (var i = 0; i < sorter.getElements().length; i++) {
       assert.equal(sorter.getElements()[i], aSimpleArraySorted[i]);
     }
@@ -43,5 +45,92 @@ describe('Bubble Sorting', function () {
     assert.equal(sorter.getElements()[0], objectArray[1]);
     assert.equal(sorter.getElements()[1], objectArray[0]);
     assert.equal(sorter.getElements()[2], objectArray[2]);
+
+    assert.equal(sorter.sortBy('.userId').bubbleSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.height').bubbleSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[0]);
+    assert.equal(sorter.getElements()[1], objectArray[2]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.yearsUntilGraduation').bubbleSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+  });
+});
+
+describe('Selection Sort', function () {
+  it('Should sort the simple array correctly', function () {
+    var sorter = new Sorter(aSimpleArray);
+
+    assert.equal(sorter.selectionSort().length, 6);
+
+    for (var i = 0; i < sorter.getElements().length; i++) {
+      assert.equal(sorter.getElements()[i], aSimpleArraySorted[i]);
+    }
+  });
+
+  it('Should sort the object array by the selected property path', function () {
+    var sorter = new Sorter(objectArray);
+
+    assert.equal(sorter.sortBy('.age').selectionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[1]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[2]);
+
+    assert.equal(sorter.sortBy('.userId').selectionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.height').selectionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[0]);
+    assert.equal(sorter.getElements()[1], objectArray[2]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.yearsUntilGraduation').selectionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+  });
+});
+
+describe('Insertion Sort', function () {
+  it('Should sort the simple array correctly', function () {
+    var sorter = new Sorter(aSimpleArray);
+
+    assert.equal(sorter.insertionSort().length, 6);
+
+    for (var i = 0; i < sorter.getElements().length; i++) {
+      assert.equal(sorter.getElements()[i], aSimpleArraySorted[i]);
+    }
+  });
+
+  it('Should sort the object array by the selected property path', function () {
+    var sorter = new Sorter(objectArray);
+
+    assert.equal(sorter.sortBy('.age').insertionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[1]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[2]);
+
+    assert.equal(sorter.sortBy('.userId').insertionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.height').insertionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[0]);
+    assert.equal(sorter.getElements()[1], objectArray[2]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
+
+    assert.equal(sorter.sortBy('.yearsUntilGraduation').insertionSort().length, 3);
+    assert.equal(sorter.getElements()[0], objectArray[2]);
+    assert.equal(sorter.getElements()[1], objectArray[0]);
+    assert.equal(sorter.getElements()[2], objectArray[1]);
   });
 });
