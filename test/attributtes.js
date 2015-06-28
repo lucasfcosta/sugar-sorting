485948be3295c83713abde5621683073b318f7ec
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2015-06-28 12:55:17 -0300

    Removing .toggleSimpleArray() and using simple var

--- test/attributtes.js
+++ test/attributtes.js
@@ -9,10 +9,4 @@ describe('Sorter Properties', function () {
       var sorter = new Sorter(aSimpleArray);
       assert.equal(sorter.getElements(), aSimpleArray);
   });
-
-  it('Should toggle simpleArray property and check if was really done', function () {
-      var sorter = new Sorter(aSimpleArray);
-      assert.equal(sorter.toggleSimpleArray(), false);
-      assert.equal(sorter.toggleSimpleArray(), true);
-  });
 });
