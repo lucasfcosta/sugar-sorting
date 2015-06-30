7de83ec95b159b55f0ca63232ce9057318f1278b
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2015-06-30 11:55:52 -0300

    New fillWith tests

--- test/attributtes.js
+++ test/attributtes.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var Sorter = require('../lib/sorter.js');
 
 var aSimpleArray = [2,6,5,1,4,3];
+var newArray = [9,10,8];
 
 describe('Sorter Properties', function () {
   it('Should return the array elements inserted on the sorter object', function () {
@@ -10,6 +11,20 @@ describe('Sorter Properties', function () {
 
       for (var i = 0; i < sorter.getElements().length; i++) {
         assert.equal(sorter.getElements()[i], aSimpleArray[i]);
+      }
+  });
+
+  it('Should return the elements inserted using fillWith()', function () {
+      var sorter = new Sorter(aSimpleArray);
+
+      for (var i = 0; i < sorter.getElements().length; i++) {
+        assert.equal(sorter.getElements()[i], aSimpleArray[i]);
+      }
+
+      sorter.fillWith(newArray);
+
+      for (var j = 0; j < sorter.getElements().length; j++) {
+        assert.equal(sorter.getElements()[j], newArray[j]);
       }
   });
 
@@ -23,6 +38,14 @@ describe('Sorter Properties', function () {
       // Checks if aSimpleArray is still the same as before sorting it
       for (var i = 0; i < aSimpleArray.length; i++) {
         assert.equal(beforeSorting[i], aSimpleArray[i]);
+      }
+
+      // Fills with a new array and checks if it references it or duplicates it
+      sorter.fillWith(newArray);
+      beforeSorting = newArray.slice(0);
+      sorter.bubbleSort();
+      for (var j = 0; j < aSimpleArray.length; j++) {
+        assert.equal(beforeSorting[j], newArray[j]);
       }
   });
 });
