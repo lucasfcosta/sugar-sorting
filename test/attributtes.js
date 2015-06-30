1fc0b6d31db44b90ff07a4a4139b4aa4f76b39fc
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2015-06-30 10:38:21 -0300

    Saving elements as a new array instead of a reference

--- test/attributtes.js
+++ test/attributtes.js
@@ -7,6 +7,9 @@ var aSimpleArray = [2,6,5,1,4,3];
 describe('Sorter Properties', function () {
   it('Should return the array elements inserted on the sorter object', function () {
       var sorter = new Sorter(aSimpleArray);
-      assert.equal(sorter.getElements(), aSimpleArray);
+
+      for (var i = 0; i < sorter.getElements().length; i++) {
+        assert.equal(sorter.getElements()[i], aSimpleArray[i]);
+      }
   });
 });
