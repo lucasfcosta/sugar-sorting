6e2f1fbdd9f80c4ed3c259c9a298f9547d927dcd
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-01-05 18:13:56 -0200

    Test files using ES6

--- test/sorting.js
+++ test/sorting.js
@@ -1,11 +1,11 @@
 'use strict';
 const assert = require('chai').assert;
-var Sorter = require('../lib/sorter.js');
+const Sorter = require('../lib/sorter.js');
 
-var aSimpleArray = [2,6,5,1,4,3];
-var aSimpleArraySorted = [1, 2, 3, 4, 5, 6];
+let aSimpleArray = [2,6,5,1,4,3];
+let aSimpleArraySorted = [1, 2, 3, 4, 5, 6];
 
-var objectArray = [{
+let objectArray = [{
     name: 'Vin Diesel',
     age: 28,
     userId: 78,
@@ -36,16 +36,16 @@ var objectArray = [{
     }
 }];
 
-describe.skip('Bubble Sort', function () {
-  it('Should sort the simple array correctly', function () {
-    var sorter = new Sorter(aSimpleArray);
+describe.skip('Bubble Sort', () => {
+  it('Should sort the simple array correctly', () => {
+    let sorter = new Sorter(aSimpleArray);
 
     assert.strictEqual(sorter.bubbleSort().length, 6);
     assert.strictEqual(sorter.getElements(), aSimpleArraySorted);
   });
 
-  it('Should sort the object array by the selected property path', function () {
-    var sorter = new Sorter(objectArray);
+  it('Should sort the object array by the selected property path', () => {
+    let sorter = new Sorter(objectArray);
 
     assert.strictEqual(sorter.sortBy('.age').bubbleSort().length, 3);
     assert.deepEqual(sorter.getElements()[0], objectArray[1]);
@@ -68,8 +68,8 @@ describe.skip('Bubble Sort', function () {
     assert.deepEqual(sorter.getElements()[2], objectArray[1]);
   });
 
-  it('Should sort nested properties', function () {
-    var sorter = new Sorter(objectArray);
+  it('Should sort nested properties', () => {
+    let sorter = new Sorter(objectArray);
 
     assert.strictEqual(sorter.sortBy('.family.numberOfChildren').bubbleSort().length, 3);
     assert.deepEqual(sorter.getElements()[0], objectArray[1]);
